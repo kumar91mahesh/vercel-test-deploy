8de6bf529f286c44a72fe95d5d8af31faee0efb1
kumar91mahesh <kumar91mahesh@gmail.com>
2024-08-17 22:22:46 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>My mk test </title>
+        <title>My mk test 2 </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
